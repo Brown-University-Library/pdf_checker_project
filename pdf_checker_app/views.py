58d5afadc1bbe80a3304fdc7b53a2883298f2aa2
Birkin James Diana <birkin.diana@gmail.com>
2026-01-10 10:14:22 -0500

    applies linter changes.

--- pdf_checker_app/views.py
+++ pdf_checker_app/views.py
@@ -10,12 +10,12 @@ from django.contrib import messages
 from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from pdf_checker_app.lib import version_helper
 from django.urls import reverse
-from pdf_checker_app.lib.version_helper import GatherCommitAndBranchData
-from pdf_checker_app.forms import PDFUploadForm
-from pdf_checker_app.models import PDFDocument
 
+from pdf_checker_app.forms import PDFUploadForm
+from pdf_checker_app.lib import version_helper
+from pdf_checker_app.lib.version_helper import GatherCommitAndBranchData
+from pdf_checker_app.models import PDFDocument
 
 log = logging.getLogger(__name__)
 
@@ -66,9 +66,7 @@ def error_check(request):
     log.debug(f'project_settings.DEBUG, ``{project_settings.DEBUG}``')
     if project_settings.DEBUG is True:  # localdev and dev-server; never production
         log.debug('triggering exception')
-        raise Exception(
-            'Raising intentional exception to check email-admins-on-error functionality.'
-        )
+        raise Exception('Raising intentional exception to check email-admins-on-error functionality.')
     else:
         log.debug('returning 404')
         return HttpResponseNotFound('<div>404 / Not Found</div>')
@@ -129,11 +127,11 @@ def save_temp_file(file: UploadedFile, checksum: str) -> Path:
     temp_dir = Path(project_settings.BASE_DIR) / 'tmp' / 'uploads'
     temp_dir.mkdir(parents=True, exist_ok=True)
     temp_path = temp_dir / f'{checksum}.pdf'
-    
+
     with open(temp_path, 'wb') as dest:
         for chunk in file.chunks():
             dest.write(chunk)
-    
+
     return temp_path
 
 
@@ -146,22 +144,20 @@ def upload_pdf(request):
         form = PDFUploadForm(request.POST, request.FILES)
         if form.is_valid():
             pdf_file = form.cleaned_data['pdf_file']
-            
+
             ## Get Shibboleth user info
             user_info = get_shibboleth_user_info(request)
-            
+
             ## Generate checksum
             checksum = generate_checksum(pdf_file)
-            
+
             ## Check if already processed
-            existing_doc = PDFDocument.objects.filter(
-                file_checksum=checksum
-            ).first()
-            
+            existing_doc = PDFDocument.objects.filter(file_checksum=checksum).first()
+
             if existing_doc and existing_doc.processing_status == 'completed':
                 messages.info(request, 'This PDF has already been processed.')
                 return HttpResponseRedirect(reverse('pdf_report_url', kwargs={'pk': existing_doc.pk}))
-            
+
             ## Create new document record with Shibboleth user info
             if not existing_doc:
                 doc = PDFDocument.objects.create(
@@ -172,25 +168,23 @@ def upload_pdf(request):
                     user_last_name=user_info['last_name'],
                     user_email=user_info['email'],
                     user_groups=user_info['groups'],
-                    processing_status='pending'
+                    processing_status='pending',
                 )
             else:
                 doc = existing_doc
-            
+
             ## Save temporary file for processing
             temp_path = save_temp_file(pdf_file, checksum)
             log.debug(f'saved temp file to {temp_path}')
-            
+
             ## TODO: Process with veraPDF (will be implemented separately)
             ## For now, just mark as pending and redirect
             messages.success(request, 'PDF uploaded successfully and queued for processing.')
             return HttpResponseRedirect(reverse('pdf_report_url', kwargs={'pk': doc.pk}))
     else:
         form = PDFUploadForm()
-    
-    return render(request, 'pdf_checker_app/upload.html', {
-        'form': form
-    })
+
+    return render(request, 'pdf_checker_app/upload.html', {'form': form})
 
 
 def view_report(request, pk: int):
@@ -199,10 +193,14 @@ def view_report(request, pk: int):
     """
     log.debug(f'starting view_report() for pk={pk}')
     doc = get_object_or_404(PDFDocument, pk=pk)
-    
+
     ## TODO: Implement full report display logic
     ## For now, just show basic document info and status
-    
-    return render(request, 'pdf_checker_app/report.html', {
-        'document': doc,
-    })
+
+    return render(
+        request,
+        'pdf_checker_app/report.html',
+        {
+            'document': doc,
+        },
+    )
